1e72a92015d0a815e9ccd1e8c3bde43ddaa628f4
TITAN COMMUNITY <153067412+TITANASSOCIATION@users.noreply.github.com>
2024-04-23 21:50:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,10 +10,10 @@ import time
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, PICS, CONFIRM_ID_CHNL
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, PICS, CONFIRM_ID_CHNL, SUDO_USERS, IS_VERIFY, VERIFY_EXPIRE, SHORTLINK_API, TUT_VID, PREMIUM_USERS, PREMIUM_CHANNEL 
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
