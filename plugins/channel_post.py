572457ff2e5cd0601314a4e3fc9e81e87079bb22
TITAN COMMUNITY <153067412+TITANASSOCIATION@users.noreply.github.com>
2024-05-13 14:03:48 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,7 +9,7 @@ from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','ban_user','unban_user','banlist', 'help', 'settimer', 'checktimer', 'restart']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','info','plans','users','broadcast','batch','genlink','stats','ban_user','unban_user','banlist', 'help', 'restart']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...!", quote = True)
     try:
