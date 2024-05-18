4498795e5a14c6a94c5541be03ff5b8d93940ee9
TITAN COMMUNITY <153067412+TITANASSOCIATION@users.noreply.github.com>
2024-05-18 22:24:58 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,7 +3,7 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3
+from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL2
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
@@ -38,29 +38,11 @@ async def is_subscribed(filter, client, update):
         return False
     else:
         return True
-
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL3:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL3, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
         
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
     if not FORCE_SUB_CHANNEL2:
-        return True
-    if not FORCE_SUB_CHANNEL3:
         return True
     
     user_id = update.from_user.id
@@ -77,11 +59,6 @@ async def is_subscribed(filter, client, update):
     
     try:
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL2, user_id=user_id)
-    except UserNotParticipant:
-        return False
-
-    try:
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL3, user_id=user_id)
     except UserNotParticipant:
         return False
     else:
