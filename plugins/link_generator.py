5fe2842204e5710c9866ecdbcc1f72c0fadd9d6b
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2024-07-04 16:21:11 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,8 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, shortner_url, shortner_api
 from helper_func import encode, get_message_id
+from cloudscraper import create_scraper
+from urllib.parse import quote
+
+async def short_url(url):
+    c = create_scraper().request
+    r = c('GET', f'https://{shortner_url}/api?api={shortner_api}&url={quote(url)}').json()
+    return (r['shortenedUrl'])
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
@@ -34,6 +41,7 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/Titan_Files_Ind_Bot?start={base64_string}"
+    link = await short_url(link)
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
@@ -54,5 +62,6 @@ async def link_generator(client: Client, message: Message):
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/Titan_Files_Ind_Bot?start={base64_string}"
+    link = await short_url(link)
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
