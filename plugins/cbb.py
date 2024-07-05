f90c9e61382c26fcd171e63fa7faba7ff0bb47d7
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2024-07-05 14:05:22 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -95,12 +95,11 @@ async def cb_handler_func(client, query: CallbackQuery):
         msg = query.message
         await query.answer('Checking.......')
         is_req = await Force_Sub(client, msg, query=True)
-        print("Query response :- " + is_req)
-        if not is_req:
+    
+        if is_req:
             await query.answer("Thanks for subscribing, Now you can use me!", show_alert=True)          
-        elif is_req:
+        else:
             await query.answer("First join both of the channels then click here!", show_alert=True)
-           
             
     elif data == "close":
         await query.message.delete()
