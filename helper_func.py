9f3a18abbcff4ea396c9f047b9d291c351642fbb
Titan_Cinemas <titancinemasindia@gmail.com>
2024-07-21 20:22:42 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,41 +4,25 @@ import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 from config import *
+from database.join_req import *
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, update):
+    user_id = update.from_user.id    
     if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = update.from_user.id
+        return False
     
     if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
- 
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-        
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
+        return True    
     try:
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL, user_id=user_id)
+        if member.status in {ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER}:
+            if await db.find_join_req(user_id):
+                return True
+        return False
     except UserNotParticipant:
         return False
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
 
 async def encode(string):
     string_bytes = string.encode("ascii")
